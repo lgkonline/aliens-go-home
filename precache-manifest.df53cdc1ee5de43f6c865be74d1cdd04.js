5f157227efbb154076da0eacbff4f8477fe5c2e0
Lars Gerrit Kliesing <lgk@patorg.org>
2020-01-31 11:37:22 +0100

    Updates

---
+++ precache-manifest.df53cdc1ee5de43f6c865be74d1cdd04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4ab6c525747bb68781b743c23a4b2e6",
+    "url": "/aliens-go-home/index.html"
+  },
+  {
+    "revision": "2ff09a7a861349617c47",
+    "url": "/aliens-go-home/static/css/main.b6db9690.chunk.css"
+  },
+  {
+    "revision": "d8f51c6d5809062e7e2a",
+    "url": "/aliens-go-home/static/js/2.8a972963.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/aliens-go-home/static/js/2.8a972963.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ff09a7a861349617c47",
+    "url": "/aliens-go-home/static/js/main.39ecc6d3.chunk.js"
+  },
+  {
+    "revision": "600513ddeb4d10666edb",
+    "url": "/aliens-go-home/static/js/runtime-main.195035e9.js"
+  }
+]);
