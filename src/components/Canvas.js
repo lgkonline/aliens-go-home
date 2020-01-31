dd23dd8c109661da67ce8d5bfcc3dbb41f82ef72
Lars Gerrit Kliesing <lgk@patorg.org>
2020-01-31 11:53:29 +0100

    improvements

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -4,6 +4,9 @@ import Sky from './Sky';
 import Ground from './Ground';
 import CannonPipe from './CannonPipe';
 import CannonBase from './CannonBase';
+import CannonBall from './CannonBall';
+import CurrentScore from './CurrentScore';
+import FlyingObject from './FlyingObject';
 
 const Canvas = ({ angle, trackMouse }) => {
     const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
@@ -15,9 +18,20 @@ const Canvas = ({ angle, trackMouse }) => {
             onMouseMove={trackMouse}
             viewBox={viewBox}
         >
+            <defs>
+                <filter id="shadow">
+                    <feDropShadow dx="1" dy="1" stdDeviation="2" />
+                </filter>
+            </defs>
+
             <Sky />
             <Ground />
             <CannonPipe rotation={angle} />
+            <CannonBall position={{ x: 0, y: -100 }} />
+            <CurrentScore score={0} />
+
+            <FlyingObject position={{ x: -150, y: -300 }} />
+            <FlyingObject position={{ x: 150, y: -300 }} />
             <CannonBase />
         </svg>
     );
