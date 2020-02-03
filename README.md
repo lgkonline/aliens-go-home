bdcfdaa07f59515478799c0caa51b26c6dbcb139
Lars Gerrit Kliesing <lgk@patorg.org>
2020-02-03 12:06:06 +0100

    More

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Aliens go home - Game created with React
 
-I followed this tutorial: [https://auth0.com/blog/developing-games-with-react-redux-and-svg-part-2/#Creating.the.Heart.React.Component](https://auth0.com/blog/developing-games-with-react-redux-and-svg-part-2/#Creating.the.Heart.React.Component)
+I followed this tutorial: [https://auth0.com/blog/developing-games-with-react-redux-and-svg-part-2/#Enabling-Users-to-Start-the-Game](https://auth0.com/blog/developing-games-with-react-redux-and-svg-part-2/#Enabling-Users-to-Start-the-Game)
--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -7,9 +7,12 @@ import CannonBase from './CannonBase';
 import CannonBall from './CannonBall';
 import CurrentScore from './CurrentScore';
 import FlyingObject from './FlyingObject';
+import StartGame from './StartGame';
+import Title from './Title';
 
-const Canvas = ({ angle, trackMouse }) => {
-    const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
+const Canvas = ({ angle, trackMouse, gameState }) => {
+    const gameHeight = 1200;
+    const viewBox = [window.innerWidth / -2, 100 - gameHeight, window.innerWidth, gameHeight];
 
     return (
         <svg
@@ -28,10 +31,20 @@ const Canvas = ({ angle, trackMouse }) => {
             <Ground />
             <CannonPipe rotation={angle} />
             <CannonBall position={{ x: 0, y: -100 }} />
-            <CurrentScore score={0} />
+            <CurrentScore score={15} />
 
-            <FlyingObject position={{ x: -150, y: -300 }} />
-            <FlyingObject position={{ x: 150, y: -300 }} />
+            {gameState?.started ?
+                <g>
+                    <FlyingObject position={{ x: -150, y: -300 }} />
+                    <FlyingObject position={{ x: 150, y: -300 }} />
+                </g>
+                :
+                <g>
+                    <StartGame onClick={() => console.log('Aliens, go home!')} />
+                    <Title />
+                </g>
+            }
+
             <CannonBase />
         </svg>
     );
@@ -39,7 +52,13 @@ const Canvas = ({ angle, trackMouse }) => {
 
 Canvas.propTypes = {
     angle: PropTypes.number.isRequired,
+    gameState: PropTypes.shape({
+        started: PropTypes.bool.isRequired,
+        kills: PropTypes.number.isRequired,
+        lives: PropTypes.number.isRequired,
+    }).isRequired,
     trackMouse: PropTypes.func.isRequired,
+    startGame: PropTypes.func.isRequired
 };
 
 export default Canvas;
